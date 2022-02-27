d70f97534263b47ec111b06234923ef61aba7256
PinoPaprika <benjamin.gerbe@gmail.com>
2022-02-27 17:24:28 +0100

    ajout rocket + balistic + vent

--- LibPhx.py
+++ LibPhx.py
@@ -2,7 +2,6 @@ import sys
 import pygame
 import math
 
-
 def scalaire(A, B, C):
     return (B[0] - A[0]) * (C[0] - A[0]) + (B[1] - A[1]) * (C[1] - A[1])
 
@@ -14,6 +13,13 @@ def normalise(vec):
 def normalized(vec):
     normal = normalise(vec)
     return [vec[0] / normal, vec[1] / normal]
+
+def lerp(a,b,c):
+    return (c*a) + ((1-c) * b)
+
+def clamp(current,_min,_max):
+    return max(min(_max,current),_min)
+
 
 
 def getAngle(A, B):
@@ -44,6 +50,10 @@ class body:
     def getVelocity(self):
         return self.velocity
 
+    def setWind(self,x,y):
+        self.wind[0] = x
+        self.wind[1] = y
+
     def getPosition(self):
         return self.currentPosition;
 
@@ -65,18 +75,18 @@ class body:
         self.Force = 0
         self.oldPosition = self.currentPosition
         self.vitesse = [0, 0]
-
-
+        self.simulationSpeed = 0.1
+        self.wind = [0,0]
 
     def addForce(self, angle, Force):
         self.angle = angle
         self.Force = Force
 
     def simulate(self):
-        self.pas += 0.1
+        self.pas += self.simulationSpeed
 
-        V = (self.Force * math.cos(math.radians(self.angle))) + self.velocity[0]
-        W = ((-self.gravity *self.pas) + self.Force * math.sin(math.radians(self.angle)) ) + self.velocity[1]
+        V = (self.Force * math.cos(math.radians(self.angle))) + self.velocity[0] + self.wind[0]
+        W = ((-self.gravity *self.pas) + self.Force * math.sin(math.radians(self.angle)) ) + self.velocity[1] + self.wind[1]
         self.vitesse = [V, W]
         self.currentPosition = [V * self.pas + (self.x),
                                 (-1 / 2 * self.gravity * self.pas ** 2) +( W * self.pas) + (self.y)]
@@ -114,7 +124,8 @@ class body:
 
             if(Force > 1):
                 self.addForce(-getAngle(A,symetrie),((Force)) * coefRebond)
-
+            return True;
+        return False;
 
     def findSurface(self, decore, decorerect, x_, y_):
 
@@ -125,8 +136,6 @@ class body:
 
             y = int(max(0, min(decorerect.height - 1, y)))
             x = int(max(0, min(decorerect.width - 1, x)))
-
-
 
             color = decore.get_at((x, y + self.pivot[1]))
 
@@ -155,13 +164,16 @@ class Collider(body):
         return str(self.x) + "/" + str(self.y)
 
     def debuggerBounds(self):
-        x = int(max(0, min(self.rect.width - 1, self.pivot[0])))
+        x =int(max(0, min(self.rect.width - 1, self.pivot[0])))
         y = int(max(0, min(self.rect.height - 1, self.pivot[1])))
 
         self.image.set_at((x, y), (255, 0, 0, 255))
 
 
 class Worms(Collider):
+
+    MaxHitDammage = 80
+    MinDistanceHit = 100
 
     def __init__(self, x, y, image, rect, Equipe):
         Collider.__init__(self, x, y, image, rect)
@@ -170,12 +182,35 @@ class Worms(Collider):
         self.Arrow = image
         self.Angle = 0
         self.tir = Collider(0, 0, image, rect)
+        self.life = 100
+        self.lifebar = pygame.Surface((100, 10), pygame.SRCALPHA)
+
+
+
+
+
+
+    def calculedommage(self,x,y):
+        vector = [self.currentPosition[0] - x,self.currentPosition[1] - y]
+        distance = normalise(vector)
+        value = distance/Worms.MinDistanceHit;
+        value = clamp(value,0,1)
+        dammage = lerp(0,Worms.MaxHitDammage,abs(value))
+
+        self.life = self.life - dammage
+
+        if(self.life < 0):
+            self.life  = 0;
+
+
+
+
 
     def Viser(self, Arrow, screen):
 
         self.Arrow = Arrow
 
-        A = [self.currentPosition[0] + self.pivot[0], self.currentPosition[1] + self.pivot[1] - self.pivot[1] / 2];
+        A = [self.currentPosition[0] + self.pivot[0], self.currentPosition[1]];
         Mouse = pygame.mouse.get_pos()
 
         self.Angle = getAngle(A, Mouse)
@@ -187,12 +222,19 @@ class Worms(Collider):
         ABnorm = normalized([Mouse[0] - A[0], Mouse[1] - A[1]])
         screen.blit(self.Arrow, [A[0] + ABnorm[0] * 50 - self.Arrow.get_rect().width / 2,
                                  A[1] + ABnorm[1] * 50 - self.Arrow.get_rect().height / 2])
-        return self.currentPosition + [self.Angle]
+
+        return A  + [self.Angle]
+
+    def getLife(self):
+        return self.life;
+
+    def get_Equipe(self):
+        return self.Equipe;
 
     def draw(self, screen, font):
         Collider.draw(self, screen)
 
-        textsurface = font.render(self.Equipe, False, (0,0, 0))
+        textsurface = font.render(self.Equipe.get_nom(), False, (0,0, 0))
 
         if self.velocity[0] > 0 and not self.flip:
             self.flip = True;
@@ -203,7 +245,17 @@ class Worms(Collider):
             self.flip = False
             self.image = pygame.transform.flip(self.image, True, False)
 
-        screen.blit(textsurface, (self.currentPosition[0], self.currentPosition[1] - 20))
+
+        for i in range(0,(int(self.lifebar.get_rect().width))):
+            for j in range(0, self.lifebar.get_rect().height):
+                self.lifebar.set_at((int(i),int(j)),(0,0,0,0))
+
+        for i in range(0,(int(self.life))):
+            for j in range(0, self.lifebar.get_rect().height):
+                self.lifebar.set_at((int(i),int(j)),self.Equipe.get_Couleur())
+
+        screen.blit(self.lifebar, (self.currentPosition[0] - (self.lifebar.get_rect().width/2) + self.pivot[0], self.currentPosition[1] - 20))
+        screen.blit(textsurface, (self.currentPosition[0] - (textsurface.get_rect().width/2) + self.pivot[0], self.currentPosition[1] - 40))
 
     def move(self):
         keys = pygame.key.get_pressed()
@@ -214,3 +266,35 @@ class Worms(Collider):
 
         if (keys[pygame.K_LEFT]):
             self.Setvelocity(-5, 0)
+
+
+class balistic(body):
+
+
+    def __init__(self,x,y,simulationspeed,angle,force,couleur):
+        body.__init__(self,x,y)
+        self.simulationSpeed = simulationspeed;
+        self.arrayOfPos = []
+        self.Force = force;
+        self.Angle = angle;
+        self.rayTex = pygame.Surface((10, 10), pygame.SRCALPHA)
+
+        for i in range(0, (int(self.rayTex.get_rect().width))):
+            for j in range(0, self.rayTex.get_rect().height):
+                dst = math.sqrt((i - 5) ** 2 + (j - 5) ** 2)
+                if(dst < 2):
+                    self.rayTex.set_at((int(i), int(j)), couleur)
+
+
+    def line(self,decore,coefrebond):
+        x = 0
+        body.addForce(self,self.Angle,self.Force)
+        while( x < 1000):
+            body.simulate(self)
+            body.collision(self,decore,decore.get_rect(),coefrebond)
+            self.arrayOfPos.append((self.currentPosition[0],self.currentPosition[1]))
+            x+=1
+
+    def draw(self,screen):
+        for i in self.arrayOfPos:
+            screen.blit(self.rayTex, (i))
--- main.py
+++ main.py
@@ -6,6 +6,19 @@ import pygame
 import LibPhx
 from random import randrange
 
+class Equipe:
+    def __init__(self,nom,couleur):
+        self.nom = nom;
+        self.couleur = couleur;
+
+    def get_Couleur(self):
+        return self.couleur;
+
+    def get_nom(self):
+        return self.nom;
+
+
+
 # initialisation de pygame :
 pygame.init()
 size = width, height = 1440, 900
@@ -13,27 +26,35 @@ black = 0, 0, 0
 screen = pygame.display.set_mode(size)
 pygame.font.init()
 myfont = pygame.font.SysFont('Comic Sans MS', 10)
-
+myfont2 = pygame.font.SysFont('Comic Sans MS', 25)
 
 # le décor
 decore =  pygame.Surface((1440, 900), pygame.SRCALPHA)
+lifebarEquipe =  pygame.Surface((100, 30), pygame.SRCALPHA)
 arrow = pygame.image.load("Images/Arrow.png")
+arrowRocket = pygame.image.load("Images/ArrowRocket.png")
+arrowWind = pygame.image.load("Images/ArrowWind.png")
 fond = pygame.image.load("Images/fond.png")
 decorerect = decore.get_rect()
+
+
 
 
 
 nbWormsEquipe = 1
 
 QueuePlay = []
-Equipes = ['Equipe 1','Equipe 2','Equipe 3']
+Equipes = [Equipe('Equipe 1',(255,0,0,255)),Equipe('Equipe 2',(0,0,255,255)),Equipe('Equipe 3',(255,255,0,255))]
+
+
 listOfWorms = []
 EquipeIdx = 0
 for i in range(0,nbWormsEquipe*len(Equipes)):
     EquipeIdx+=1
     worms = pygame.image.load("Images/worms.png")
     wormsrect = worms.get_rect()
-    listOfWorms.append( LibPhx.Worms(randrange(0,screen.get_width()) , 500 ,worms,wormsrect,Equipes[ EquipeIdx % len(Equipes)])  )
+
+    listOfWorms.append( LibPhx.Worms(randrange(0,screen.get_width()) , 500 ,worms,wormsrect,Equipes[ EquipeIdx % len(Equipes)]) )
 
 for i in listOfWorms:
     QueuePlay.append(i)
@@ -42,13 +63,25 @@ currentWorms = QueuePlay.pop()
 QueuePlay.append(currentWorms)
 
 Clock = pygame.time.Clock()
+AngleWind = randrange(float(0),float(360));
+
+arrowWind = pygame.transform.rotate(arrowWind, -AngleWind - 90)
+
+windForce=5;
 
 viser = True
 InfoTir = [0,0,0]
 grenade = None
+rocket = None
 
-timer = 100
-counter = timer
+timerGranade = 100
+timerRocket = 0
+
+selectRocket = False;
+selectGrenade = True;
+
+pressed = False;
+killed = False;
 
 for i in range(0, decorerect.width):
     for j in range(int(decorerect.height/2), (decorerect.height)):
@@ -71,6 +104,14 @@ def destroyDecor(x,y):
             if(dst < 100):
                 decore.set_at((i,j),(255,255,255,0))
 
+def findWorms(_currentWorms):
+    _currentWorms.Setvelocity(0, 0)
+    _currentWorms = QueuePlay.pop(0)
+    QueuePlay.append(_currentWorms)
+
+    return _currentWorms
+
+
 
 while 1:
     # pour fermer la fenêtre
@@ -83,26 +124,68 @@ while 1:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_e:
-                currentWorms.Setvelocity(0, 0)
-                currentWorms = QueuePlay.pop(0)
-                QueuePlay.append(currentWorms)
-                viser = True
+                pass
+                #pressed = True;
+
+            if event.key == pygame.K_g:
+                selectGrenade = True;
+                selectRocket = False\
+
+            if event.key ==  pygame.K_r:
+                selectRocket = True;
+                selectGrenade = False;
+
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             viser = False
-            grenade = LibPhx.Collider(InfoTir[0],InfoTir[1], pygame.image.load("Images/Arrow.png"),pygame.image.load("Images/Arrow.png").get_rect())
-            grenade.addForce(InfoTir[2],100)
-            counter = timer
+            if(selectRocket):
+                imgrocket = pygame.image.load("Images/rocket.png");
+                imgrocket = pygame.transform.rotate(imgrocket, InfoTir[2] )
+                rocket = LibPhx.Collider(InfoTir[0],InfoTir[1], imgrocket,imgrocket.get_rect());
+                rocket.addForce(-InfoTir[2], 300)
+                counter = timerRocket
+
+            if(selectGrenade):
+                grenade = LibPhx.Collider(InfoTir[0],InfoTir[1], pygame.image.load("Images/W4_Grenade.png"),pygame.image.load("Images/W4_Grenade.png").get_rect())
+                grenade.addForce(-InfoTir[2],100)
+                counter = timerGranade
 
 
     screen.fill(black)
 
+
+
+
     screen.blit(fond, fond.get_rect())
     screen.blit(decore, decorerect)
+    k = 0
+    for i in Equipes:
 
-    for i in listOfWorms:
-        i.simulate();
-        i.collision(decore, decorerect, 0.8)
+        life = 0;
+        for j in listOfWorms:
+            if (i == j.get_Equipe()):
+                life = life + j.getLife()
+        textsurface = myfont2.render(i.get_nom() + " :" + str(int(life)), False, i.get_Couleur())
+
+        screen.blit(textsurface, ( 0,50 * k))
+        k+=1
+
+
+    if rocket != None:
+        rocket.draw(screen)
+        rocket.simulate()
+        val = rocket.collision(decore, decorerect, 0.8)
+        rocket.setWind(5 * math.cos(AngleWind),5 * math.sin(AngleWind))
+
+        if(val ):
+            pos = rocket.getPosition()
+            destroyDecor(int(pos[0]), int(pos[1]))
+            rocket = None
+            pressed = True;
+            for i in listOfWorms:
+                i.calculedommage(pos[0], pos[1])
+
+
 
 
     if grenade != None:
@@ -110,24 +193,63 @@ while 1:
         grenade.simulate()
         grenade.collision(decore, decorerect, 0.8)
 
-
-
         counter -= 1
 
+        grenade.setWind(5 * math.cos(AngleWind), 5 * math.sin(AngleWind))
+
         if(counter < 0):
-            pos = grenade.getPosition();
+            pos = grenade.getPosition()
             destroyDecor(int(pos[0]),int(pos[1]))
             grenade = None
-
-
+            pressed = True;
+            for i in listOfWorms:
+                i.calculedommage(pos[0],pos[1])
 
 
     for i in listOfWorms:
-        i.draw(screen, myfont);
-        if(i == currentWorms):
-            i.move()
-            i.debuggerBounds()
-            if viser:
-                InfoTir = i.Viser(arrow, screen)
+
+        if( i == currentWorms and i.getLife()<= 0):
+            killed = True;
+
+        if(i.getLife() > 0):
+            i.draw(screen, myfont)
+            i.simulate();
+            i.collision(decore, decorerect, 0.8)
+
+            if(i == currentWorms ):
+                    i.move()
+                    i.debuggerBounds()
+
+                    if viser:
+
+                        if(selectGrenade):
+                            balistic = LibPhx.balistic(InfoTir[0], InfoTir[1], 0.01, -InfoTir[2], 100, currentWorms.get_Equipe().get_Couleur());
+                            balistic.setWind(windForce * math.cos(AngleWind), windForce * math.sin(AngleWind))
+                            balistic.line(decore, 0.8)
+                            balistic.draw(screen)
+
+                            InfoTir = i.Viser(arrow, screen)
+                        elif(selectRocket) :
+                            balistic = LibPhx.balistic(InfoTir[0], InfoTir[1], 0.01, -InfoTir[2], 300, currentWorms.get_Equipe().get_Couleur());
+                            balistic.setWind(windForce * math.cos(AngleWind), windForce * math.sin(AngleWind))
+                            balistic.line(decore, 0)
+                            balistic.draw(screen)
+
+                            InfoTir = i.Viser(arrowRocket, screen)
+
+
+    if(pressed or killed):
+        currentEquipe = currentWorms.get_Equipe().get_nom()
+        currentWorms = findWorms(currentWorms)
+
+        if (currentWorms.getLife() <= 0 ):
+            currentWorms = findWorms(currentWorms)
+
+        viser = True
+        pressed = False;
+        killed = False;
+
+    screen.blit(arrowWind, (screen.get_width()-80,25))
 
     pygame.display.flip()
+
